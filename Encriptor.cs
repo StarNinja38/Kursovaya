42cd8ec9167e0c976e607868e8f8f52bca02af3f
StarNinja <38291161+StarNinja38@users.noreply.github.com>
2025-05-29 10:08:12 +0300

    Create Encriptor.cs

---
+++ Encriptor.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text;
+
+namespace DataProtectionApp
+{
+    public class Encryptor
+    {
+        private readonly string key;
+
+        public Encryptor(string key)
+        {
+            this.key = key;
+        }
+
+        // Метод шифрования текста
+        public string Encrypt(string plainText)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char encryptedChar = (char)(plainText[i] ^ key[i % key.Length]);
+                result.Append(encryptedChar);
+            }
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(result.ToString()));
+        }
+
+        // Метод дешифрования текста
+        public string Decrypt(string cipherText)
+        {
+            string decoded = Encoding.UTF8.GetString(Convert.FromBase64String(cipherText));
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < decoded.Length; i++)
+            {
+                char decryptedChar = (char)(decoded[i] ^ key[i % key.Length]);
+                result.Append(decryptedChar);
+            }
+            return result.ToString();
+        }
+    }
+}
